499d568c84708c673244020318339fba786eba65
Edward Ahn <edhyah@gmail.com>
2022-06-07 14:13:26 -0700

    Aframe globe component airline routes demo working

--- flights/index.js
+++ flights/index.js
@@ -4,11 +4,11 @@ var app = express();
 require('dotenv').config({ path: './config.env' });
 const port = process.env.PORT || 5000;
 
-app.use(express.static('public'));
+//app.get('/home', function (req, res) {
+//    res.sendFile(__dirname + '/index.html');
+//});
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`App is running on port: ${port}`);
