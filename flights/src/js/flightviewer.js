947eaf4d059d022142caeb55a5a12f9bc5aa86e7
Edward Ahn <edhyah@gmail.com>
2022-06-08 22:30:04 -0700

    Use triggers to rotate globe

--- flights/src/js/flightviewer.js
+++ flights/src/js/flightviewer.js
@@ -3,6 +3,8 @@ AFRAME.registerComponent('flightviewer', {
         var el = this.el;
 
         this.onThumbstickMoved = this.onThumbstickMoved.bind(this);
+        this.onTriggerDown = this.onTriggerDown.bind(this);
+        this.onTriggerUp = this.onTriggerUp.bind(this);
 
         this.initCameraRig();
 
@@ -10,10 +12,15 @@ AFRAME.registerComponent('flightviewer', {
         this.globeEl.id = 'globe';
         this.globeEl.object3D.scale.set(0.1, 0.1, 0.1);
         this.globeEl.setAttribute('globe', {'globeImageUrl': '//unpkg.com/three-globe/example/img/earth-night.jpg'})
+        this.globeEl.classList.add('raycastable');
         el.appendChild(this.globeEl);
 
         this.leftHandEl.addEventListener('thumbstickmoved', this.onThumbstickMoved);
         this.rightHandEl.addEventListener('thumbstickmoved', this.onThumbstickMoved);
+        this.leftHandEl.addEventListener('triggerdown', this.onTriggerDown);
+        this.rightHandEl.addEventListener('triggerdown', this.onTriggerDown);
+        this.leftHandEl.addEventListener('triggerup', this.onTriggerUp);
+        this.rightHandEl.addEventListener('triggerup', this.onTriggerUp);
 
         this.cameraRigEl.object3D.position.set(0, 0, 25);
         this.cameraRigEl.setAttribute('movement-controls', {fly: true, speed: 0.5});
@@ -33,9 +40,9 @@ AFRAME.registerComponent('flightviewer', {
         });
 
         leftHandEl.setAttribute('laser-controls', {hand: 'left'});
-        leftHandEl.setAttribute('raycaster', {objects: ['globe'], interval: 100, lineColor: 'steelblue', lineOpacity: 0.85});
+        leftHandEl.setAttribute('raycaster', {objects: ['.raycastable'], interval: 100, lineColor: 'steelblue', lineOpacity: 0.85});
         rightHandEl.setAttribute('laser-controls', {hand: 'right'});
-        rightHandEl.setAttribute('raycaster', {objects: ['globe'], interval: 100, lineColor: 'steelblue', lineOpacity: 0.85});
+        rightHandEl.setAttribute('raycaster', {objects: ['.raycastable'], interval: 100, lineColor: 'steelblue', lineOpacity: 0.85});
 
         cameraRigEl.appendChild(cameraEl);
         cameraRigEl.appendChild(leftHandEl);
@@ -48,9 +55,34 @@ AFRAME.registerComponent('flightviewer', {
         var globeScale = this.globeScale || this.globeEl.object3D.scale.x;
         globeScale -= evt.detail.y / 20;
         globeScale = Math.min(Math.max(0.1, globeScale), 0.25);
-        console.log(globeScale);
         this.globeEl.object3D.scale.set(globeScale, globeScale, globeScale);
         this.globeScale = globeScale;
-        console.log(this.cameraRigEl.getAttribute('position'));
+    },
+
+    onTriggerDown: function (evt) {
+        this.activeHandEl = evt.srcElement;
+    },
+
+    onTriggerUp: function (evt) {
+        this.oldHandX = undefined;
+        this.oldHandY = undefined;
+        this.activeHandEl = undefined;
+    },
+
+    tick: function () {
+        if (!this.el.sceneEl.is('vr-mode') || !this.activeHandEl) { return; }
+
+        var intersection = this.activeHandEl.components.raycaster.getIntersection(this.globeEl);
+        if (!intersection) { return; }
+
+        var intersectionPosition = intersection.point;
+        this.oldHandX = this.oldHandX || intersectionPosition.x;
+        this.oldHandY = this.oldHandY || intersectionPosition.y;
+
+        this.globeEl.object3D.rotation.y -= (this.oldHandX - intersectionPosition.x) / 4;
+        this.globeEl.object3D.rotation.x += (this.oldHandY - intersectionPosition.y) / 4;
+
+        this.oldHandX = intersectionPosition.x;
+        this.oldHandY = intersectionPosition.y;
     }
 });
